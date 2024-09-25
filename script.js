050550b5ee540500be965ece2cb612002d6135d4
jackrichter <jackrichter1@gmail.com>
2024-09-25 16:18:30 +0200

    AJAX via XML/HTTP completed.

--- script.js
+++ script.js
@@ -6,11 +6,43 @@ const countriesContainer = document.querySelector('.countries');
 ///////////////////////////////////////
 
 // AJAX calls the XML/http way
-const request = new XMLHttpRequest();
-request.open('GET', 'https://restcountries.com/v3.1/name/portugal');
-request.send();
+const getCountryData = country => {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
 
-request.addEventListener('load', function () {
-  const data = JSON.parse(this.responseText);
-  console.log(data);
-});
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+
+    // console.log(data);
+    // console.log(Object.values(data.languages));
+    // console.log(Object.values(data.currencies)[0].name);
+    // console.log(data.flags.svg);
+    // console.log(data.currencies.EUR.name);
+
+    const html = `
+    <article class="country">
+      <img class="country__img" src="${data.flags.svg}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name.common}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)} mil people</p>
+        <p class="country__row"><span>🗣️</span>${
+          Object.values(data.languages)[0]
+        }</p>
+        <p class="country__row"><span>💰</span>${
+          Object.values(data.currencies)[0].name
+        }</p>
+      </div>
+    </article>
+  `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('portugal');
+getCountryData('usa');
+getCountryData('sweden');
