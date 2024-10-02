536e6ff06362e68590ecb3e489b1a2301f53afb3
jackrichter <jackrichter1@gmail.com>
2024-10-02 02:28:59 +0200

    Promisefying image loading functionality (challenge #2)

--- challenge2.js
+++ challenge2.js
@@ -32,3 +32,51 @@ PART 2
 TEST DATA: Images in the img folder. Test the error handler by passing a wrong image path.
 Set the network speed to 'Fast 3G' in the dev tools Network tab, otherwise images load too fast.
 */
+
+const imageContainer = document.querySelector('.images');
+
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const createImage = function (imagePath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imagePath;
+
+    img.addEventListener('load', () => {
+      imageContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', () => {
+      reject(new Error('Image not found'));
+    });
+  });
+};
+
+let currentImage;
+
+createImage('img/img-1.jpg')
+  .then(img => {
+    currentImage = img;
+    console.log('Image 1 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImage.style.display = 'none';
+    return createImage('img/img-2.jpg');
+  })
+  .then(img => {
+    currentImage = img;
+    console.log('Image 2 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImage.style.display = 'none';
+  })
+  .catch(err => console.error(err));
+
+///////////////////////////////////////
